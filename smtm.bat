494afedd5cf9f8baa4e412bb985d6d6baeaa3462
Seyfemichael Teklemariam <welcomesmtm@gmail.com>
2022-11-17 10:15:25 +0300

    Add files via upload

---
+++ smtm.bat
@@ -0,0 +1,6 @@
+# Script to run BurpSuite Pro 
+# Sunday, ‎December ‎19, ‎2021, ‏‎1:45:32 PM
+# Author SMTM
+java  -noverify --illegal-access=permit -javaagent:burploader.jar -jar burpsuite_pro_v2021.8.4.jar
+pause >null
+#java -noverify --illegal-access=permit -javaagent:burploader.jar -jar burpsuite_pro_v2021.8.4.jar
